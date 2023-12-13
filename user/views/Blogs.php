5719d468d70418e6eeba43825515a2acc41c3bff
AnhMinh0203 <Leanhminh0203@gmail.com>
2023-12-13 11:52:53 +0700

    Minh: Done blogs

---
+++ user/views/Blogs.php
@@ -0,0 +1,299 @@
+<?php require('includes/header.html'); ?>
+<div class="header-destination ">
+    <div class="content">
+        <h1 class="text-center fw-bold text-light">Blog</h1>
+        <div class="row">
+            <h2 class="text-center dirc_destination">
+                <a href="#" class="header-destination__home text-light">Home</a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right text-danger" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+                    <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+                <span class="header-destination__destination text-light">Blog</span>
+            </h2>   
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="blog-items">
+        <div class="row">
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-1.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-2.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-3.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-4.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-5.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-6.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-7.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-8.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="blog-item">
+                    <div class="blog-item-img">
+                        <img src="../assets/img/blogs/blog-md-9.png" alt="" class="hover-zoom">
+                    </div>
+                    <div class="blog-item-content">
+                        <a href="#" class="blog-item-writer">
+                            <div class="blog-writer-icon">
+                                <i class="fa-solid fa-circle-user blog-item-icon"></i>
+                            </div>
+                            <div class="blog-writer-name">By John Smith</div>
+                        </a>
+                        <a href="#" class="blog-item-date">
+                            <div class="blog-date-icon">
+                                <i class="fa-regular fa-calendar blog-item-icon"></i>
+                            </div>
+                            <div class="blog-date-time">Novembar 16, 2021</div>
+                        </a>
+                    </div>
+                    <div class="blog-item-title">
+                        <p> sollicitudin urna metus iaculis odio eget efficitur leo eros id lectus.</p>
+                    </div>
+                    <div class="blog-travle">
+                        <a href="#">Tourist</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="blog-pagination">
+        <ul class="blog-list">
+            <li class="blog-pagination-item blog-page-arrow">
+                <a href="#">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </a>
+            </li>
+            <li class="blog-pagination-item">
+                <a href="#">01</a> 
+            </li>
+            <li class="blog-pagination-item">
+                <a href="#">02</a> 
+            </li>
+            <li class="blog-pagination-item">
+                <a href="#">03</a> 
+            </li>
+            <li class="blog-pagination-item">
+                <a href="#">04</a> 
+            </li>
+            <li class="blog-pagination-item blog-page-arrow">
+                <a href="#">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<?php require('includes/footer.html'); ?>
+
+
+
+

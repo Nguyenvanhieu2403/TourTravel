b99e02c14ebe368a9232aeee839f0ee1d871810c
xMenJr <20212181@eaut.edu.vn>
2023-12-14 17:02:17 +0700

    Quan

--- user/views/Destination.php
+++ user/views/Destination.php
@@ -1,20 +1,8 @@
 <?php require('includes/header.html'); ?>
-<div class="header-destination ">
-    <div class="content">
-        <h1 class="text-center fw-bold text-light">Destination</h1>
-        <div class="row">
-            <h2 class="text-center dirc_destination">
-                <a href="#" class="header-destination__home text-light">Home</a>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right text-danger" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
-                    <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-                <span class="header-destination__destination text-light">Destination</span>
-            </h2>
-        </div>
-    </div>
-</div>
+<?php require('includes/navbar.html'); ?>
 
-<h1>test</h1>
+<div>
+    <h1>Test</h1>
+</div>
 
 <?php require('includes/footer.html'); ?>
